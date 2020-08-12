b606364ef4cf1bd60e007fde7f494c136c3fe782
tanvirch0750 <tanvir0750@yahoo.com>
2020-08-12 22:13:13 +0600

    Updates

---
+++ precache-manifest.984e2e3bdb168ef1d347d1f0c6036925.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7d46f8970d9b979609c255cf16233743",
+    "url": "/tanvirch0750/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "d34fc2670d4ad4db33ae",
+    "url": "/tanvirch0750/monsters-rolodex/static/css/main.6bfee23b.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/tanvirch0750/monsters-rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/tanvirch0750/monsters-rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d34fc2670d4ad4db33ae",
+    "url": "/tanvirch0750/monsters-rolodex/static/js/main.f00257de.chunk.js"
+  },
+  {
+    "revision": "37d84ad7778cca672bd1",
+    "url": "/tanvirch0750/monsters-rolodex/static/js/runtime-main.3585be23.js"
+  }
+]);
